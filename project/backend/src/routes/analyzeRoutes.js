2b4206be9649299aba5b9615a8b3f98d4eb2903a
AjaySingh312 <ajaysinghandnikhil@gmail.com>
2026-02-26 16:54:33 +0530

    Delete project directory

--- project/backend/src/routes/analyzeRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { analyzeAudioController } from '../controllers/analyzeController.js';
-import upload from '../middleware/uploadMiddleware.js';
-
-const router = Router();
-
-router.post('/analyze', upload.single('audio'), analyzeAudioController);
-
-export default router;
